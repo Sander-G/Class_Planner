ba2a6c4f9ff6336d3e3cc1015c62ed6b0446e5a5
Sander - MacBook Pro <Sander@Grunnekemeijer.net>
2023-05-01 13:35:58 +0200

    setup firebase / router / base pages

--- planner-app/src/App.jsx
+++ planner-app/src/App.jsx
@@ -1,14 +1,22 @@
 
 import './App.css'
-
+import { Routes, Route } from 'react-router-dom'
+import Login from './components/Login'
+import Admin from './components/Admin'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
 function App() {
-
 
   return (
     <>
-      <h1>Class Planner</h1>
+      <Routes>
+        <Route exact path='/' element={<Login />} />
+        <Route exact path='/admin' element={<Admin />} />
+        <Route exact path='/home' element={<Home />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App
