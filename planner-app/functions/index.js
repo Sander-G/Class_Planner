a6d0a5d6fe3197000f4cdec7b63bbfa47f3235b9
Sander - iMac <Sander@Grunnekemeijer.net>
2023-05-03 10:04:58 +0200

    firebase netlify serverless setup

--- planner-app/functions/index.js
+++ planner-app/functions/index.js
@@ -1,9 +1,6 @@
-const functions = require("firebase-functions");
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+const API_KEY = "AIzaSyD2ZgzBPERK6ZrGNJbxIx3mhF8DdH7oASg";
+
+module.exports = {API_KEY,
+};
