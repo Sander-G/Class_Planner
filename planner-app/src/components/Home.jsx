10018f08cc4c275b085114068760ba65065c7e8b
Sander - MacBook Pro <Sander@Grunnekemeijer.net>
2023-05-05 22:08:01 +0200

    calendar localization

--- planner-app/src/components/Home.jsx
+++ planner-app/src/components/Home.jsx
@@ -1,20 +1,22 @@
+
 import { useState, useEffect } from 'react';
 import { initializeFirebaseApp } from '../../firebaseConfig';
 import { collection, getDocs } from 'firebase/firestore';
-
-
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment';
-import 'moment/locale/nl';
-const localizer = momentLocalizer(moment);
-
+import 'moment/locale/nl'; 
 import Modal from 'react-modal';
 import ClassInfoPopup from './ClassInfoPopup';
+
+moment.locale('nl');
+const localizer = momentLocalizer(moment);
+
 Modal.setAppElement('#root');
 
-
 export default function Home() {
+ 
+ 
   const [events, setEvents] = useState([]);
   const [selectedEvent, setSelectedEvent] = useState(null);
 
@@ -32,11 +34,10 @@ export default function Home() {
           max_spots: data.max_spots,
           enrolled_users: data.enrolled_users,
           available_spots: data.available_spots,
-          
         };
       });
-       
-       console.log('Classes:', classesData);
+
+      console.log('Classes:', classesData);
       setEvents(classesData);
     };
     fetchClasses();
@@ -45,12 +46,10 @@ export default function Home() {
   const handleEventSelect = (event) => {
     setSelectedEvent(event);
     console.log('Selected event:', event);
-   
   };
   const handleClosePopup = () => {
     setSelectedEvent(null);
   };
-
 
   const handleLogout = async () => {
     try {
@@ -62,15 +61,34 @@ export default function Home() {
     }
   };
 
- 
+  const messages = {
+    allDay: 'Hele dag',
+    previous: '<',
+    next: '>',
+    today: 'Vandaag',
+    month: 'Maand',
+    week: 'Week',
+    day: 'Dag',
+    agenda: 'Agenda',
+    date: 'Datum',
+    time: 'Tijd',
+    event: 'Gebeurtenis',
+    noEventsInRange: 'Geen gebeurtenissen in dit bereik.',
+    showMore: (total) => `+ ${total} meer`,
+  };
 
-
+  const formats = {
+    monthHeaderFormat: (date, culture, localizer) => localizer.format(date, 'MMMM YYYY', culture),
+    dayFormat: (date, culture, localizer) => localizer.format(date, 'D MMMM', culture),
+  };
+   
   return (
     <div>
       <h2>Lotta Yoga Planner - Overzicht</h2>
       <Calendar
         localizer={localizer}
-        culture='nl'
+        messages={messages}
+        formats={formats}
         min={new Date(0, 0, 0, 8)} // 8:00 AM
         max={new Date(0, 0, 0, 22)} // 10:00 PM
         step={45}
@@ -88,24 +106,23 @@ export default function Home() {
   );
 }
 
-
- const modalStyles = {
-   content: {
-     top: '50%',
-     left: '50%',
-     right: 'auto',
-     bottom: 'auto',
-     marginRight: '-50%',
-     transform: 'translate(-50%, -50%)',
-     backgroundColor: '#525252',
-     padding: '2rem',
-     zIndex: '7000',
-     position: 'fixed',
-     border: 'none',
-     boxShadow: '0px 2px 10px rgba(0, 0, 0, 0.3)',
-     borderRadius: '4px',
-   },
-   overlay: {
-     backgroundColor: 'rgba(0, 0, 0, 0.5)',
-   },
- };
+const modalStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: '#525252',
+    padding: '2rem',
+    zIndex: '7000',
+    position: 'fixed',
+    border: 'none',
+    boxShadow: '0px 2px 10px rgba(0, 0, 0, 0.3)',
+    borderRadius: '4px',
+  },
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+};
