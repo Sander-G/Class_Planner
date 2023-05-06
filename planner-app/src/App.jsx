7a66495e58bcce9c8726d164296c93155ba80a13
Sander - MacBook Pro <Sander@Grunnekemeijer.net>
2023-05-06 14:49:48 +0200

    css cleanup

--- planner-app/src/App.jsx
+++ planner-app/src/App.jsx
@@ -9,39 +9,11 @@ import NotFound from './components/NotFound'
 import { initializeFirebaseApp } from '../firebaseConfig';
 
 function App() {
-// fetch('https://ly-firebase-cfg.netlify.app/.netlify/functions/admin', {
-//   method: 'POST',
-// })
-//   .then((response) => response.json())
-//   .then((data) => console.log(data))
-//   .catch((error) => console.error(error));
-
-
-
-  // useEffect(() => {
-  //   const getCustomClaims = async () => {
-  //     const uid = 'tpTtSmZKExeCv9rIUoZRaJX6igs2';
-  //     const response = await fetch(`https://ly-firebase-cfg.netlify.app/.netlify/functions/checkCustomClaims?uid=${uid}`);
-  //     const data = await response.json();
-  //     console.log(data);
-  //   };
-
-  //   getCustomClaims();
-  // }, []);
-
-
-
-
-
-
-
-   console.log('in app component', initializeFirebaseApp()); 
   const navigate = useNavigate();
 
  useEffect(() => {
    const fetchData = async () => {
      const { auth } = await initializeFirebaseApp();
-     console.log('auth:', auth);
      const unsubscribe = auth.onAuthStateChanged((user) => {
        if (user) {
          // User is logged in, redirect to home page
